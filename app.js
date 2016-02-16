6cbdff3c7b94645003a659c8bbada5229632c7f7
catarak <ctarakajian@gmail.com>
2016-02-16 16:24:11 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var nn = require('./libs/nn_funcs.js');
+
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.post('/image', function(req, res) {
+	console.log(req.body);
+	var input = JSON.parse(req.body.matrix);
+	nn.getNNOutput(input);
+	var outputArray = nn.allNodeOutputs;
+	// var outputArray = require('./testOutput.json');
+	res.json({"nodeOutputs": outputArray});
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
